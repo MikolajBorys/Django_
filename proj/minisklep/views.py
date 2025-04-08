811ce1e48a63763ba05d00afc4c3777e08435d5a
MikolajBorys <mikolaj.borys.9@gmail.com>
2025-04-08 21:42:58 +0200

    System logowania
    Dodano system logowania i wylogowywania oraz blokadę dodawania produktów zależnie od tego czy użytkownik jest zalogowany

--- proj/minisklep/views.py
+++ proj/minisklep/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Produkt
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+def table_prod(request):
+    produkty = Produkt.objects.all()
+    return render(request, 'table_prod.html', {'produkty': produkty})
 
+@login_required
 def add_prod(request):
     if request.method == "POST":
         Name = request.POST.get("Name")
@@ -20,6 +25,3 @@ def add_prod(request):
     return render(request, 'add_prod.html')
 
 
-def table_prod(request):
-    produkty = Produkt.objects.all()
-    return render(request, 'table_prod.html', {'produkty': produkty})
--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from minisklep.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add/', add_prod, name='add_prod'),
     path('', table_prod, name='table_prod'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=''), name='logout'),
 ]
